5da08ac1be17a934308b9604a4e3f1d18724a702
Savio Dimatteo <savio.dimatteo@sap.com>
2019-05-22 17:35:33 +0200

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,21 @@
 module.exports = {
-    "extends": "google"
+    "extends": "google",
+    "rules": {
+        "valid-jsdoc": "off",
+        "no-var": "off",
+        "max-len": "off",
+        "require-jsdoc": "off",
+        "no-multi-spaces": "off",
+        "padded-blocks": "off",
+        "comma-spacing": "off",
+        "comma-dangle": "off",
+        "spaced-comment": "off",
+        "no-unused-vars": "off",
+        "brace-style": "off",
+        "new-cap": "off",
+        "key-spacing": "off",
+        "computed-property-spacing": "off",
+        "prefer-spread": "off",
+        "no-extend-native": "off"
+    }
 };
